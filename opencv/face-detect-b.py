24124d96ea1c908f4076d0adbf976cf2a720da8d
Bergen Hung <wphuang@yahoo.com>
2024-09-19 12:37:13 -0700

    update opencv

--- opencv/face-detect-b.py
+++ opencv/face-detect-b.py
@@ -1,8 +1,8 @@
 import cv2
 
-grp_photo = cv2.imread('group-photo.webp')
+grp_photo = cv2.imread('photos/group-photo.webp')
 grayscale = cv2.cvtColor(grp_photo, cv2.COLOR_BGR2GRAY)
-# cv2.imwrite('group-photo-gray.webp', grayscale)
+# cv2.imwrite('photo/group-photo-gray.webp', grayscale)
 
 c = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 faces = c.detectMultiScale(grayscale, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
@@ -10,4 +10,4 @@ for x, y, w, h in faces:
     if w < 100 and h < 100:
         cv2.rectangle(grp_photo, (x, y), (x+w, y+h), (255, 0, 0))
 
-cv2.imwrite('group-photo-faces.webp', grp_photo)
+cv2.imwrite('photos/group-photo-faces.webp', grp_photo)
---
+++ opencv/img-b.py
@@ -0,0 +1,12 @@
+import cv2 as cv
+
+img = cv.imread('photos/cat_large.jpg')
+cv.imshow('Cat_large', img)
+
+def rescaleFrame(frame, scale=0.75)
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+cv.waitKey(0)
